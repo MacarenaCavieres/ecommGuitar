344d2ebbf0ad456a94057ba3002d4d5e2624af8b
MCavieres <maca.c.91@gmail.com>
2024-10-01 11:59:01 -0300

    se hace funciones para disminuir y aumentar cantidades

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,70 @@ import { db } from "./data/db.js";
 
 function App() {
     const [data, setData] = useState(db);
+    const [cart, setCart] = useState([]);
+
+    const MAX_ITEMS = 5;
+    const MIN_ITEMS = 1;
+
+    function addToCart(item) {
+        const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+        if (itemExists !== -1) {
+            const updateCart = [...cart];
+            updateCart[itemExists].quantity++;
+            setCart(updateCart);
+        } else {
+            item.quantity = 1;
+            setCart([...cart, item]);
+        }
+    }
+
+    function removeFromCart(id) {
+        setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+    }
+
+    function increaseQuantity(id) {
+        const updateCart = cart.map((item) => {
+            if (item.id === id && item.quantity < MAX_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1,
+                };
+            }
+            return item;
+        });
+
+        setCart(updateCart);
+    }
+
+    function decreaseQuantity(id) {
+        const updateCart = cart.map((item) => {
+            if (item.id === id && item.quantity > MIN_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1,
+                };
+            }
+            return item;
+        });
+        setCart(updateCart);
+    }
 
     return (
         <>
-            <Header />
+            <Header
+                cart={cart}
+                removeFromCart={removeFromCart}
+                increaseQuantity={increaseQuantity}
+                decreaseQuantity={decreaseQuantity}
+            />
 
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
                     {data.map((guitar) => (
-                        <Guitar guitar={guitar} />
+                        <Guitar key={guitar.id} guitar={guitar} setCart={setCart} addToCart={addToCart} />
                     ))}
                 </div>
             </main>
