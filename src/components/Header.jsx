5b837abf2fc55d75c05d80ffd94990af573d8113
MCavieres <maca.c.91@gmail.com>
2024-10-01 14:13:17 -0300

    se modifica ruta de img para el carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div className="carrito">
-                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
                                 {isEmpty ? (
